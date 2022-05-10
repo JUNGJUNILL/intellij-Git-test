29e7717c3f3405b8e71caa718836f20bed3b045b
JUNGJUNILL <dala1207@naver.com>
2022-05-10 23:33:35 +0900

    commit_test_20220510

---
+++ src/HelloGItPushTest.java
@@ -0,0 +1,6 @@
+public class HelloGItPushTest {
+
+    public static void main(String[] args) {
+        System.out.println("반가워요 전 이것을 커밋 푸쉬 할 겁니다.!");
+    }
+}
--- src/HelloIntellij.java
+++ src/HelloIntellij.java
@@ -2,5 +2,6 @@ public class HelloIntellij {
 
     public static void main(String[] args) {
         System.out.println("안녕하세요dd");
+        System.out.println("반갑습니다.");
     }
 }
