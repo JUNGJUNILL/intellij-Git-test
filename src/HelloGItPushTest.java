9a77b64c42214682c4c30639a6440989a589f581
JUNGJUNILL <dala1207@naver.com>
2022-05-10 23:46:07 +0900

    commit_update_20220510_11:46

--- src/HelloGItPushTest.java
+++ src/HelloGItPushTest.java
@@ -2,5 +2,6 @@ public class HelloGItPushTest {
 
     public static void main(String[] args) {
         System.out.println("반가워요 전 이것을 커밋 푸쉬 할 겁니다.!");
+        System.out.println("업데이트 칠 겁니다.");
     }
 }
